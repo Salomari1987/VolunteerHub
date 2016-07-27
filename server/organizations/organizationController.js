9ec279f2ea58b45b7591bbce1394653a6cebdbbe
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-07-27 16:08:25 +0300

    Implemented a function for updating organization's info

--- server/organizations/organizationController.js
+++ server/organizations/organizationController.js
@@ -47,5 +47,37 @@ module.exports = {
         res.status(500).send('No such organization exists');
       }
     });
+  },
+
+  editProfile : function(req,res){
+    Organization.findOne({_id: req.params.id.toString()})
+    .exec(function(error, organization){
+      if(error){
+        res.status(500).send(error);
+      } else if(!organization){
+        res.status(500).send(new Error ('Organization not added Yet'));
+      } else {
+
+        organization.EIN = req.body.EIN || organization.EIN;
+        organization.name = req.body.name || organization.name;
+        organization.causes_area = req.body.causes_area || organization.causes_area;
+        organization.locations = req.body.locations || organization.locations;
+        organization.missionStatement = req.body.missionStatement || organization.missionStatement;
+        organization.contactInfo = req.body.contactInfo || organization.contactInfo;
+        organization.rate = req.body.rate || organization.rate;
+        organization.picture = req.body.picture || organization.picture;
+        organization.currentOpportunities = req.body.currentOpportunities || organization.currentOpportunities;
+        organization.pastOpportunities = req.body.pastOpportunities || organization.pastOpportunities;
+        organization.owners = req.body.owners || organization.owners;
+
+        organization.save(function(err, savedOrg){
+          if(err){
+            res.status(500).send(error);
+          } else {
+            res.status(201).send('Info Updated!');
+          }
+        });
+      }
+    });
   }
 };
