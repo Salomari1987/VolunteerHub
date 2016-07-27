a37d70858b2aa0cd501ddcc891919717dbc70ae0
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-27 20:09:10 +0400

    Revamp model schema

--- server/openings/OpeningModel.js
+++ server/openings/OpeningModel.js
@@ -1,9 +1,15 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var openingSchema = new mongoose.Schema({
   title : {
   	type : String,
   	required : true
+  },
+  _owner : { 
+  	type: mongoose.Schema.Types.ObjectId,
+  	ref: 'Opportunity',
+    required: true
   },
   numberOfVolunteers: Number,
   location : String,
@@ -14,4 +20,4 @@ var openingSchema = new mongoose.Schema({
 });
 var Opening = mongoose.model( 'Opening', openingSchema );
 
-module.exports = Opportunity;
+module.exports = Opening;
