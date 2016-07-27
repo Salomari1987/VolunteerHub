7e9717e0dd42715a249a6b7237aca2a17389d27c
Saif Elokour <saiffelokour@gmail.com>
2016-07-27 20:14:22 +0300

    changed console log message

--- server/server.js
+++ server/server.js
@@ -5,20 +5,21 @@ var user=require('./events/eventModel.js');
 
 var app = express();
 
-// connect to mongo database named "khatwa"
+
+// connect to mongo database named "VolunteerHub"
 mongoose.connect('mongodb://localhost/VolunteerHub');
 db=mongoose.connection;
 db.once('open',function () {
-	console.log('mongo db is open');
+	console.log('mongoDB is open');
 });
 
 // configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
-// start listening to requests on port 8000
+// start listening to requests on port 8080
 app.listen(8080);
-console.log('listen to ');
+console.log('listen to 8080');
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
