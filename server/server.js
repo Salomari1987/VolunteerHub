f529ffc17279d1b764f62b2e32454c01ab8346bb
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-27 16:49:30 +0400

    rename DB to VolunteerHub

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var user=require('./users/userModel.js');
-var user=require('./events/eventModel.js');
+var user=require('./opportunities/opportunityModel.js');
 
 var app = express();
 
