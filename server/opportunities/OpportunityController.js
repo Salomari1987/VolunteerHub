fc723857172c7210ed3bdfd8c8ec5c84c7aa4301
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-27 20:09:59 +0400

    Get all and get one

--- server/opportunities/OpportunityController.js
+++ server/opportunities/OpportunityController.js
@@ -1,4 +1,4 @@
-var Opportunity=require('./OpporunityModel.js');
+var Opportunity=require('./OpportunityModel.js');
 var User=require('../users/userModel.js');
 var Q = require('q');
 var jwt = require('jwt-simple');
@@ -7,6 +7,10 @@ var Opening = require('../openings/openingModel')
 var findOpportunity = Q.nbind(Opportunity.findOne, Opportunity);
 var createOpportunity = Q.nbind(Opportunity.create, Opportunity);
 var findAllOpportunities = Q.nbind(Opportunity.find, Opportunity);
+
+var findOpening = Q.nbind(Opening.findOne, Opening);
+var createOpening = Q.nbind(Opening.create, Opening);
+var findAllOpening = Q.nbind(Opening.find, Opening);
 
 module.exports ={
 
@@ -30,14 +34,12 @@ module.exports ={
 			// locationId : req.body.locationId,
 			type : req.body.type,
 			description : req.body.description,
-			requiredSkills : req.body.requiredSkills,
+  			skillsRequired: req.body.skillsRequired,
 			poster : req.body.poster
 	  	}
 	  	createOpportunity(tempOpportunity)
 	  		.then(function (createdOpporunity) {
 		        if (createdOpporunity) {
-		        	console.log('event created');
-		        	console.log(createdOpporunity);
 		          res.json(createdOpporunity);
 		        }
 		      })
@@ -49,19 +51,79 @@ module.exports ={
 
   	//Test : Get
   	//http://127.0.0.1:8000/api/event/5790db44f97a940c03550a89
-
-  	getOpportunity : function (req,res,next) {
-
-  		 console.log(req.params.id);
-  		 var id=(req.params.id).toString();
-
-  		Opportunity.findOne({_id: id}, function(err, doc) {
-  			if(err)
-  				res.status(500).send(err);
-		  res.status(200).send(doc);
-		});
-  		
+  	editOpportunity : function (req,res,next) {
+  		var opId = req.body.opId;
+  		var token = req.headers['x-access-token'];
+  		// if (!token){
+  		// 	next(new Error('No token'))
+  		// } else {
+  			findOpportunity({_id:opId})
+  			.then(function (opportunity) {
+  				if(!opportunity) {
+  					next(new Error('Opportunity does not exist'));
+  				} else {
+  					opportunity.title = req.body.title || opportunity.title;
+  					opportunity.startDate = req.body.startDate || opportunity.startDate;
+  					opportunity.endDate = req.body.endDate || opportunity.endDate;
+  					opportunity.location = req.body.location || opportunity.location;
+  					opportunity.type = req.body.type || opportunity.type;
+  					opportunity.description = req.body.description || opportunity.description;
+  					opportunity.requiredSkills = req.body.requiredSkills || opportunity.requiredSkills;
+  					opportunity.poster = req.body.poster || opportunity.poster;
+  					opportunity.save();
+  					res.json(opportunity);
+  				}
+  			})
+  		// }
   	},
+  	addOpening: function (req, res, next) {
+  		var opportunityId = req.params.id
+  		var currOpening = {
+  			title: req.body.title,
+  			_owner: opportunityId,
+  			numberOfVolunteers: req.body.numberOfVolunteers,
+  			location: req.body.location,
+  			description: req.body.description,
+  			skillsRequired: req.body.skillsRequired,
+  			resources: req.body.resources
+  		}
+  		createOpening(currOpening)
+	  	.then(function (newOpening) {
+		    if (newOpening) {
+		    	findOpportunity ( { _id:opportunityId } )
+		    	.then(function (opportunity) {
+			    	if(!opportunity) {
+	  					next(new Error('Opportunity does not exist'));
+	  				} else {
+	  					opportunity.openings.push(newOpening._id)
+	  					opportunity.save();
+	  					return opportunity.openings
+	  				}
+	  			})
+	  			.then(function(openings) {
+	  				findAllOpening({'_id': { $in: openings}})
+			        .then(function(allOpenings){
+			          res.json(allOpenings);
+			        })
+	  			})
+	  			.fail(function(err){
+		        	next(err)
+		      	})
+		   	}})
+	    .fail(function (error) {
+	        next(error);
+	 	})
+	},
+  	getOpportunity : function (req,res,next) {
+  		var id=(req.params.id).toString();
+  		findOpportunity({_id: id}) 
+  		.then (function(opportunity) {
+		  	res.status(200).send(opportunity);
+		})
+		.fail(function(error) {
+			next(error);
+		})
+  	}
 
 
   	// Test : Post
