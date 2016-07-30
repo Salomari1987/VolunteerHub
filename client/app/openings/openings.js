7163edfb0855ac44ef38f5e9e885a5c18cf9490d
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-07-30 13:17:27 +0300

    Made progress of the organizations/opportunities front-end

--- client/app/openings/openings.js
+++ client/app/openings/openings.js
@@ -1,16 +1,24 @@
 angular.module('VolunteerHub.openings', [])
 .controller('OpeningsCtrl', function ($scope, Openings,$location, Auth) {
-	$scope.signout = function(){
-		Auth.signout();
-	}
 
 	$scope.userId = window.userId;
+	
 	$scope.data={};
-	Openings.getAll()
-	.then(function(openings){
-		$scope.data.openings = openings;
-	})
-	.catch(function(error){
-		console.error(error)
-	})
+	
+	$scope.intialize = function(){
+		Openings.getAll()
+		.then(function(openings){
+			$scope.data.openings = openings;
+		})
+		.catch(function(error){
+			console.error(error)
+		})
+	};
+
+	$scope.apply = function(openingId){
+		//TODO
+		console.log(openingId);
+	};
+
+	$scope.intialize();
 });
