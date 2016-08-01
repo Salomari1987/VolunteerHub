1702590f2288097be5affd28955b1b104566911d
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-01 19:42:37 +0300

    adding headers to responses

--- server/server.js
+++ server/server.js
@@ -15,17 +15,17 @@ db.once('open',function () {
 var app = express();
 // configure our server with all the middleware and routing
 var port = process.env.PORT || 8000;
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Accept');
+    next();
+});
 
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 // start listening to requests on port 8000
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 
 app.listen(port, function () {
   console.log(' app listening on port ' + port);
@@ -34,18 +34,18 @@ app.listen(port, function () {
 //Kills server connection if it crashes or killed
 //this is important so not to keep the 8000 port busy
 //If the app crashes
-app.on('uncaughtException', function(){
-	//Close connection
-	server.close();
-})
-// On kill
-app.on('SIGTERM', function(){
-	server.close();
-})
-//On exit
-app.on('exit', function(){
-	server.close();
-})
+// app.on('uncaughtException', function(){
+// 	//Close connection
+// 	server.close();
+// })
+// // On kill
+// app.on('SIGTERM', function(){
+// 	server.close();
+// })
+// //On exit
+// app.on('exit', function(){
+// 	server.close();
+// })
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
