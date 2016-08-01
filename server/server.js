cb68d537314200226af3c5d232213e92239901f3
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-01 19:08:23 +0300

    adding headers to responses

--- server/server.js
+++ server/server.js
@@ -20,6 +20,13 @@ require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 // start listening to requests on port 8000
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.listen(port, function () {
   console.log(' app listening on port ' + port);
 });
